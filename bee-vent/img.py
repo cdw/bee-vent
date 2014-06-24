c0fcd46fcc1596267deb77ef4f06697e6a190a8f
Dave Williams <cdave@uw.edu>
2014-06-24 19:04:16 -0400

    Only rotate image on display, else sharpness lost.

--- bee-vent/img.py
+++ bee-vent/img.py
@@ -11,27 +11,35 @@ import numpy as np
 
 
 class BeeImg(object):
+    """In this class, rotation is stored as an angle by which the bee is
+    rotated counter clockwise from facing up the screen. The image is only
+    rotated on being displayed as repeated rotations of an image degrade it;
+    that is, if we were to repeatedly apply rotations to the image, rather 
+    than rotating it only when it's displayed, the image would lose sharpness.
+    """
     def __init__(self, img_loc = './bee.png'):
-        self.img = Image.open(img_loc)
+        self._img = Image.open(img_loc)
         self._rotation = 0
 
     def rotate(self, deg = 0):
+        """Rotate the image to the specified angle."""
+        self._rotation = deg
+
+    def rotate_by(self, deg = 0):
         """Rotate the image counter clockwise by X degrees."""
         self._rotation += deg  # to be able to reset
-        self.img = self.img.rotate(deg)
 
     def reset_rotation(self):
         """Rotate back to our original orientation."""
-        self.img = self.img.rotate(-self._rotation)
-        self._rotation -= self._rotation
+        self._rotation = 0
 
     def img(self):
         """Return the image as a PIL image instance."""
-        return self.img
+        return self._img.rotate(self._rotation)
 
     def array(self):
         """Return the image as a numpy array."""
-        return np.array(self.img)
+        return np.array(self._img.rotation(self._rotation))
 
 def new_bee_img():
     return BeeImg()
